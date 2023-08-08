f19cfd469857f038efc0b087723e0c9d995bdd21
Tayyab Haseeb <73613576+tayyabhaseeb@users.noreply.github.com>
2023-08-08 23:48:37 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+    .then(res => res.json())
+    .then(data => {
+        const postsArr = data.slice(0, 5)
+        let html = ""
+        for (let post of postsArr) {
+            html += `
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+            `
+        }
+        document.getElementById("blog-list").innerHTML = html
+    })
+
+
+
+document.getElementById("new-post").addEventListener("submit", function(e) {
+    e.preventDefault()
+    const postTitle = document.getElementById("post-title").value
+    const postBody = document.getElementById("post-body").value
+    const data = {
+        title: postTitle,
+        body: postBody
+    }
+    console.log(data)
+})
